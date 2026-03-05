8b1772ac58ef6a5c3db8d3df51d3703593a3f7c7
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-03-05 05:32:18 +0000

    Makefile changes and main.cpp changes

--- main.cpp
+++ main.cpp
@@ -33,3 +33,63 @@ int main(){
 	deltStudents(students);
 	return 0;
 }
+
+void loadStudents(std::vector<Student*>& students){
+	std::ifstream file("students.csv");
+	std::string line;
+//File opening error (just in case)
+	if(!file.is_open()){
+		std::cout << "Issue opening the CSV file\n";
+		return;
+	}
+	while  (std::getline(file, line)){
+		Student* s = new Student(line);
+		students.push_back(s);
+	}
+	file.close();
+}
+//Menu function & stuff
+int menu(){
+	std::cout << "\n1. Show all student names\n";
+	std::cout << "2. Show all student data\n";
+	std::cout << "3. Search for a student\n";
+	std::cout << "0. Exit program\n";
+	std::cout << "Enter command: ";
+
+	int command = 0;
+	std::cin >> command;
+	return command:
+}
+//ShowStudentNames function 
+void showStudentNames(const std::vector<Student*>& students){
+	for (Student* s : students){
+		std::cout << s->getLastFirst() << std:: endl;
+	}
+}
+//PrintStudent function 
+void printStudents(const std::vector<Student*>& students){
+	for (Student* s : students){
+		s->printStudent();
+		std::cout << std::endl;
+	}
+}
+
+void findStudent(const std::vector<Student*>& students){
+	std::string last;
+	std::cout << "Enter last name: ";
+	std::cin >> last;
+	for (Student* s : students){
+		if (s->getLastName() == last{
+		   s->printStudent();
+		   return;
+		 }
+	}
+	std::cout << "Student not found.\n";
+}
+
+void delStudents(std::vector<Student*>& students){
+	for (Student* s : students){
+		delete s;
+	}
+	students.clear();
+}
--- makefile
+++ makefile
@@ -1,1 +1,17 @@
-#Wait for week 2
+heap: main.o student.o addresss.o date.o
+	g++ -o heap main.o student.o address.o date.o
+
+main.o: main.cpp student.h address.h date.h
+	g++ -c main.cpp
+
+student.o: student.cpp student.h address.h date.h
+	g++ -c main.cpp
+
+address.o: address.cpp address.h
+	g++ -c address.cpp
+
+date.o: date.cpp date.h
+	g++ -c date.cpp
+
+clean: 
+	rm *.o heap
